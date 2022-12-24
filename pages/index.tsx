9ab18ca2ec44e3c7b1a17b036c44ebd55e6a468d
osawa-koki <github@osawa.dev>
2022-12-24 23:47:47 +0900

    インデックスの修正。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
+import { Setting } from '../common/Setting'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout>
     <div id='Index'>
-      <h1>Hello 情報処理安全確保支援士試験 👋</h1>
+      <div id='MainTopic'>
+        <h1>Hello 情報処理安全確保支援士試験 👋</h1>
+        <img id='Logo' src={`${Setting.IMG_ROOT_PATH}/Logo.png`} alt="Logo" />
+      </div>
     </div>
   </Layout>
 )
