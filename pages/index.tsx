ced6581848d34d688d9591e3c48b9fad623002af
osawa-koki <github@osawa.dev>
2022-12-26 09:04:40 +0900

    リファクタリング。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
-import Link from 'next/link'
-import { Setting } from '../common/Setting'
-import Layout from '../components/Layout'
+import { Setting } from '../common/Setting';
+import Layout from '../components/Layout';
 
 const IndexPage = () => (
   <Layout>
@@ -9,6 +8,7 @@ const IndexPage = () => (
         <h1>Hello 情報処理安全確保支援士試験 👋</h1>
         <img id='Logo' src={`${Setting.IMG_ROOT_PATH}/Logo.png`} alt="Logo" />
       </div>
+      <div id='content'></div>
     </div>
   </Layout>
 )
